e2e6c001db2a607553825e9f3430309eb2c8be6a
bonnypanicker <bonnypanicker@outlook.com>
2026-02-21 07:56:56 +0530

    Fix duplicate context and manifest imports

--- App/Paypass/app/src/main/java/com/paypass/MainActivity.kt
+++ App/Paypass/app/src/main/java/com/paypass/MainActivity.kt
@@ -2,9 +2,7 @@ package com.paypass
 
 import android.Manifest
 import android.content.Context
-import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
